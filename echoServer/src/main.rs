be33ec3331d5ca421a43501ee7f22927f57d3a14
Vz <Vincent.Zou@qq.com>
2020-08-15 08:31:10 +0800

    code refactoring

--- echoServer/src/main.rs
+++ echoServer/src/main.rs
@@ -2,13 +2,13 @@ mod echo_server;
 
 use echo_server::EchoServer;
 
-
 /**
  * 写个简单的TcpEchoServer, 就暂时不用处异步io了
  * 线程池队列处理接收就好了
  */
 fn main() -> std::io::Result<()> {
     // 启动端口监听
+
     let mut server = EchoServer::new();
 
     server.listen("127.0.0.1:9123");
